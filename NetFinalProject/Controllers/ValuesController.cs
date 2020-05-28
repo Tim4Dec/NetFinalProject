fc3a4c13f2f97bca2914191153c0332fa7071900
ctt <939708327@qq.com>
2020-05-28 18:20:01 +0800

    init

--- NetFinalProject/Controllers/ValuesController.cs
+++ NetFinalProject/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace NetFinalProject.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "hello", "world" };
         }
 
         // GET api/values/5
